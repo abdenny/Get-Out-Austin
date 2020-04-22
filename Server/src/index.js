8e38ba0dbe441524e407a13f85c9e495e133a3fc
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-04-22 16:56:48 -0500

    next.js server pre-rendered data from database

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,33 +1,40 @@
-import 'dotenv/config';
-import cors from 'cors';
-import express from 'express';
-import db from '../models';
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+import db from "../models";
+import bodyParser from "body-parser";
 
 const app = express();
 
 app.use(cors());
 
-console.log('Now looking for user...');
+console.log("Now looking for user...");
 db.users
   .findOne({
-    where: { user_first_name: 'Austin' },
+    where: { user_first_name: "Austin" },
   })
   .then((results) => {
-    console.log('Success');
+    console.log("Success");
     console.log(results);
   });
 
-app.get('/', (req, res) => {
-  return res.send('Received a GET HTTP method');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/posts", (req, res) => {
+  console.log("recieved post from client");
+  db.posts.findAll().then((results) => {
+    res.send(JSON.stringify(results));
+  });
 });
-app.post('/', (req, res) => {
-  return res.send('Received a POST HTTP method');
+app.post("/", (req, res) => {
+  return res.send("Received a POST HTTP method");
 });
-app.put('/', (req, res) => {
-  return res.send('Received a PUT HTTP method');
+app.put("/", (req, res) => {
+  return res.send("Received a PUT HTTP method");
 });
-app.delete('/', (req, res) => {
-  return res.send('Received a DELETE HTTP method');
+app.delete("/", (req, res) => {
+  return res.send("Received a DELETE HTTP method");
 });
 
 app.listen(process.env.PORT, () => {
