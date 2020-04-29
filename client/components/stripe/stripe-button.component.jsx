eb43fa1b7a78ed4a046cb9506584e67ffe839aae
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-04-29 12:06:05 -0500

    styling individual postings

--- client/components/stripe/stripe-button.component.jsx
+++ client/components/stripe/stripe-button.component.jsx
@@ -1,10 +1,20 @@
-import React, { useContext } from 'react';
-import StripeCheckout from 'react-stripe-checkout';
-import UserContext from '../../src/context/userContext.context';
+import React, { useContext } from "react";
+import StripeCheckout from "react-stripe-checkout";
+import UserContext from "../../src/context/userContext.context";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 const StripeCheckoutButton = ({ post_price, post_id }) => {
+  const classes = useStyles();
   const { userGlobal } = useContext(UserContext);
   const priceForStripe = post_price * 100;
-  const publishableKey = 'pk_test_rs6ShmyUDyefKyUmvFkOcvk800QtJrAhp9';
+  const publishableKey = "pk_test_rs6ShmyUDyefKyUmvFkOcvk800QtJrAhp9";
 
   const onToken = (token) => {
     token = {
@@ -15,32 +25,43 @@ const StripeCheckoutButton = ({ post_price, post_id }) => {
       paid: true,
     };
     console.log(token);
-    fetch('http://localhost:3001/dbwrite/v1/posts/guests', {
-      method: 'POST',
+    fetch("http://localhost:3001/dbwrite/v1/posts/guests", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(token),
     }).then((result) => {
       return result.json();
     });
 
-    alert('Payment Succesful!');
+    alert("Payment Succesful!");
   };
 
   return (
     <StripeCheckout
-      label='Pay Now'
-      name='Get Out Austin Ltd.'
+      label="Pay Now"
+      name="Get Out Austin Ltd."
+      componentClass="div"
       billingAddress
       shippingAddress
-      image='https://getoutaustin.s3.us-east-2.amazonaws.com/Screen+Shot+2020-04-28+at+1.44.11+PM.png'
+      image="https://getoutaustin.s3.us-east-2.amazonaws.com/Screen+Shot+2020-04-28+at+1.44.11+PM.png"
       description={`Your total is $${post_price}`}
       amount={priceForStripe}
-      panelLabel='Pay Now'
+      panelLabel="Pay Now"
       token={onToken}
       stripeKey={publishableKey}
-    />
+    >
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+        className={classes.submit}
+      >
+        Purchase
+      </Button>
+    </StripeCheckout>
   );
 };
 
