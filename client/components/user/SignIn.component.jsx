a692f8fd106119d841cd427857ec8195a6448a3a
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-05-01 10:33:50 -0500

    navbar dropdown for mobile or small

--- client/components/user/SignIn.component.jsx
+++ client/components/user/SignIn.component.jsx
@@ -1,44 +1,55 @@
-import { signInWithGoogle } from '../../src/firebase.utils';
-import React, { useContext } from 'react';
-import Button from '@material-ui/core/Button';
-import Avatar from '@material-ui/core/Avatar';
-import { auth } from '../../src/firebase.utils';
-import UserContext from '../../src/context/userContext.context';
-import Link from 'next/link';
+import { signInWithGoogle } from "../../src/firebase.utils";
+import React, { useContext } from "react";
+import Button from "@material-ui/core/Button";
+import Avatar from "@material-ui/core/Avatar";
+import { auth } from "../../src/firebase.utils";
+import UserContext from "../../src/context/userContext.context";
+import Link from "next/link";
+import { makeStyles } from "@material-ui/core/styles";
+import { display } from "@material-ui/system";
+import Hidden from "@material-ui/core/Hidden";
+
+const useStyles = makeStyles((theme) => ({
+  layout: {
+    [theme.breakpoints.up("sm")]: {
+      position: "absolute",
+      right: "16px",
+      top: "12px",
+      display: "flex",
+      flexDirection: "row",
+    },
+    [theme.breakpoints.down("xs")]: {
+      // position: "absolute",
+      // right: "16px",
+      // top: "12px",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+    },
+  },
+}));
+
 const SignIn = () => {
   const { userGlobal } = useContext(UserContext);
+  const classes = useStyles();
   return (
     <>
       {userGlobal === null ? (
-        <div
-          style={{
-            position: 'absolute',
-            right: '16px',
-            display: 'flex',
-            flexDirection: 'row',
-          }}
-        >
-          <Link href='/posts'>
+        <div className={classes.layout}>
+          <Link href="/posts">
             <Button>View Posts</Button>
           </Link>
           <Button onClick={signInWithGoogle}>Login</Button>
         </div>
       ) : (
-        <div
-          style={{
-            position: 'absolute',
-            right: '16px',
-            display: 'flex',
-            flexDirection: 'row',
-          }}
-        >
-          <Link href='/posts'>
+        <div className={classes.layout}>
+          <Link href="/posts">
             <Button>View Posts</Button>
           </Link>
-          <Link href='/createpost'>
+          <Link href="/createpost">
             <Button>Create A Post</Button>
           </Link>
-          <Link href='/user'>
+          <Link href="/user">
             <Button>Account</Button>
           </Link>
           <Button
@@ -49,7 +60,9 @@ const SignIn = () => {
             Logout
           </Button>
           {/* <h3>Hello, {userGlobal.displayName}</h3> */}
-          <Avatar alt='User Image' src={userGlobal.photoURL} />
+          <Hidden xsDown implementation="css">
+            <Avatar alt="User Image" src={userGlobal.photoURL} />
+          </Hidden>
         </div>
       )}
     </>
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,22 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import theme from '../components/theme';
-import Typography from '@material-ui/core/Typography';
-import SignIn from '../components/user/SignIn.component';
-import { auth } from '../src/firebase.utils';
-import 'react-dates/initialize';
-import 'react-dates/lib/css/_datepicker.css';
-import UserContext from '../src/context/userContext.context';
-import Link from 'next/link';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import theme from "../components/theme";
+import Typography from "@material-ui/core/Typography";
+import SignIn from "../components/user/SignIn.component";
+import { auth } from "../src/firebase.utils";
+import "react-dates/initialize";
+import "react-dates/lib/css/_datepicker.css";
+import UserContext from "../src/context/userContext.context";
+import Link from "next/link";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Hidden from "@material-ui/core/Hidden";
+import Popover from "@material-ui/core/Popover";
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
   const [user, setUser] = useState(null);
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
   useEffect(() => {
     const unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
@@ -28,8 +33,19 @@ export default function MyApp(props) {
     };
   }, []);
 
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
   React.useEffect(() => {
-    const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
@@ -40,12 +56,12 @@ export default function MyApp(props) {
       <Head>
         <title>Get Out, Austin</title>
         <meta
-          name='viewport'
-          content='minimum-scale=1, initial-scale=1, width=device-width'
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
         />
         <link
-          href='https://api.mapbox.com/mapbox-gl-js/v0.51.0/mapbox-gl.css'
-          rel='stylesheet'
+          href="https://api.mapbox.com/mapbox-gl-js/v0.51.0/mapbox-gl.css"
+          rel="stylesheet"
         />
       </Head>
       <ThemeProvider theme={theme}>
@@ -53,15 +69,53 @@ export default function MyApp(props) {
         <CssBaseline />
         {/* <AppBar position='sticky'> */}
         <UserContext.Provider value={{ userGlobal: user }}>
-          <Toolbar color='primary'>
-            <Link href='/'>
-              <div style={{ cursor: 'pointer' }}>
-                <Typography variant='h6' color='inherit' noWrap>
+          <Toolbar color="primary">
+            <Link href="/">
+              <div style={{ cursor: "pointer" }}>
+                <Typography variant="h6" color="inherit" noWrap>
                   Get Out, Austin
                 </Typography>
               </div>
             </Link>
-            <SignIn currentUser={user}></SignIn>
+            <Hidden smUp implementation="css">
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                style={{
+                  position: "absolute",
+                  top: "6px",
+                  right: "16px",
+                  display: "flex",
+                  flexDirection: "row",
+                }}
+                onClick={handleClick}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Popover
+                id={id}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                anchorReference="anchorPosition"
+                anchorPosition={{ top: 56, left: 525 }}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "right",
+                }}
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "center",
+                }}
+                style={{ width: "100vw", margin: "0" }}
+              >
+                <SignIn currentUser={user}></SignIn>
+              </Popover>
+            </Hidden>
+            <Hidden xsDown implementation="css">
+              <SignIn currentUser={user}></SignIn>
+            </Hidden>
           </Toolbar>
         </UserContext.Provider>
         {/* </AppBar> */}
