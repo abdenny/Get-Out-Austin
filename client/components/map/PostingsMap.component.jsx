976cb48ac18a071ba68750f349ef338d2b338e46
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-05-03 19:01:42 -0500

    individual postings map data

--- client/components/map/PostingsMap.component.jsx
+++ client/components/map/PostingsMap.component.jsx
@@ -1,13 +1,13 @@
-import { Component } from 'react';
-import ReactMapGL, { Marker, Popup } from 'react-map-gl';
-import RoomIcon from '@material-ui/icons/Room';
-import IconButton from '@material-ui/core/IconButton';
+import { Component } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import RoomIcon from "@material-ui/icons/Room";
+import IconButton from "@material-ui/core/IconButton";
 
 class PostMap extends Component {
   state = {
     viewport: {
-      width: '600px',
-      height: '100vh',
+      width: "600px",
+      height: "100vh",
       latitude: 30.266666,
       longitude: -97.7431,
       zoom: 11,
@@ -16,13 +16,14 @@ class PostMap extends Component {
   };
 
   render() {
-    let locations = this.props.props;
+    let locations = this.props.locations;
+    console.log(locations);
     return (
       <ReactMapGL
-        mapStyle='mapbox://styles/mapbox/streets-v9'
-        mapboxApiAccessToken='pk.eyJ1IjoiYWJkZW5ueSIsImEiOiJjazlkNjNrMmUwMGRkM21sZTB0OXdseWl2In0.iQVMKfidmj4BKLbJxAot6w'
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken="pk.eyJ1IjoiYWJkZW5ueSIsImEiOiJjazlkNjNrMmUwMGRkM21sZTB0OXdseWl2In0.iQVMKfidmj4BKLbJxAot6w"
         onViewportChange={(viewport) => this.setState({ viewport })}
-        mapStyle='mapbox://styles/abdenny/ck9okzsce0kzf1iqtaqm9ddsq'
+        mapStyle="mapbox://styles/abdenny/ck9okzsce0kzf1iqtaqm9ddsq"
         {...this.state.viewport}
       >
         {locations.map((post, index) => {
--- client/components/postings/Postings.component.jsx
+++ client/components/postings/Postings.component.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import { Grid } from '@material-ui/core';
-import PostingsContent from './PostingsContent.component';
-import PostingsMap from '../map/PostingsMap.component';
-import PostToolBar from './Toolbar.components';
+import React, { useState } from "react";
+import { Grid } from "@material-ui/core";
+import PostingsContent from "./PostingsContent.component";
+import PostingsMap from "../map/PostingsMap.component";
+import PostToolBar from "./Toolbar.components";
 
 const Postings = (props) => {
   const [searchParams, setValue] = useState({
     categories: [],
     guests: 1,
     max_price: 20,
-    search: '',
+    search: "",
     wantSearch: false,
     conditionsWanted: {},
   });
@@ -20,7 +20,7 @@ const Postings = (props) => {
       categories: [],
       guests: 1,
       max_price: 20,
-      search: '',
+      search: "",
       wantSearch: false,
       conditionsWanted: {},
     });
@@ -28,14 +28,14 @@ const Postings = (props) => {
 
   let propsFilter;
   if (searchParams.wantSearch) {
-    console.log('want search', searchParams.conditionsWanted);
+    console.log("want search", searchParams.conditionsWanted);
     propsFilter = props.props.posts.filter((post) => {
       let conditionsNeeded = Object.keys(searchParams.conditionsWanted).length;
       let conditionsMet = 0;
       console.log(conditionsNeeded, conditionsMet);
       for (var key in searchParams.conditionsWanted) {
         switch (key) {
-          case 'categories':
+          case "categories":
             if (
               searchParams.conditionsWanted.categories.includes(
                 post.post_category
@@ -48,33 +48,33 @@ const Postings = (props) => {
               break;
             }
             break;
-          case 'guests':
+          case "guests":
             if (
               post.post_max_guests - post.post_booked_guests >
               parseInt(searchParams.conditionsWanted.guests)
             ) {
               conditionsMet += 1;
               break;
-            } else if (searchParams.conditionsWanted.guests === '') {
+            } else if (searchParams.conditionsWanted.guests === "") {
               conditionsMet += 1;
               break;
             }
             break;
-          case 'max_price':
+          case "max_price":
             if (
               post.post_price <
               parseInt(searchParams.conditionsWanted.max_price)
             ) {
               conditionsMet += 1;
-              console.log('max price met', conditionsMet);
+              console.log("max price met", conditionsMet);
               break;
-            } else if (searchParams.conditionsWanted.max_price === '') {
+            } else if (searchParams.conditionsWanted.max_price === "") {
               conditionsMet += 1;
               break;
             }
-            console.log('max price not met');
+            console.log("max price not met");
             break;
-          case 'search':
+          case "search":
             if (
               post.post_title
                 .toLowerCase()
@@ -89,7 +89,7 @@ const Postings = (props) => {
             break;
 
           default:
-            console.log('hello');
+            console.log("hello");
             break;
         }
       }
@@ -98,7 +98,7 @@ const Postings = (props) => {
   }
 
   return (
-    <Grid container direction='column'>
+    <Grid container direction="column">
       <Grid item container>
         <Grid item xs={12} sm={12} md={12}>
           <PostToolBar
@@ -112,7 +112,7 @@ const Postings = (props) => {
           xs={12}
           sm={12}
           md={7}
-          style={{ maxHeight: '100vh', overflow: 'auto' }}
+          style={{ maxHeight: "100vh", overflow: "auto" }}
         >
           <PostingsContent
             propsForPostingsContent={
@@ -120,8 +120,8 @@ const Postings = (props) => {
             }
           />
         </Grid>
-        <Grid item xs={false} sm={false} md={5} style={{ overflow: 'hidden' }}>
-          <PostingsMap props={props.props.posts} />
+        <Grid item xs={false} sm={false} md={5} style={{ overflow: "hidden" }}>
+          <PostingsMap locations={props.props.posts} />
         </Grid>
       </Grid>
     </Grid>
