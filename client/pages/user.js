116a9c2e5875ced3a146c782dafa6d3051fc62bc
Austin Denny <austinbdenny@gmail.com>
2020-05-04 16:54:06 -0500

    Fixed typo on ec2 endpoint.

--- client/pages/user.js
+++ client/pages/user.js
@@ -13,11 +13,11 @@ export default user;
 export async function getServerSideProps() {
   // Call an external API endpoint to get posts.
   const res = await fetch(
-    'hhttp://ec2-18-223-99-203.us-east-2.compute.amazonaws.com/api/v1/posts'
+    'http://ec2-18-223-99-203.us-east-2.compute.amazonaws.com/api/v1/posts'
   );
   const posts = await res.json();
   const res2 = await fetch(
-    'hhttp://ec2-18-223-99-203.us-east-2.compute.amazonaws.com/api/v1/posts/guests'
+    'http://ec2-18-223-99-203.us-east-2.compute.amazonaws.com/api/v1/posts/guests'
   );
   const postGuests = await res2.json();
 
