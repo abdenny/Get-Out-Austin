1aecf70a9cdd8d20bf7f27f269ecbd3f7b425170
Austin Denny <austinbdenny@gmail.com>
2020-05-05 15:00:37 -0500

    Deploy.

--- client/pages/posts.js
+++ client/pages/posts.js
@@ -10,7 +10,6 @@ const Posts = (props) => {
   );
 };
 export async function getServerSideProps() {
-  // Call an external API endpoint to get posts.
   const res = await fetch(
     'http://ec2-18-223-99-203.us-east-2.compute.amazonaws.com/api/v1/posts'
   );
