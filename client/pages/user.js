4aa1ef28973f11f67a623c07eba6259ee68c5e4b
Austin Denny <austinbdenny@gmail.com>
2020-09-09 12:37:47 -0500

    Changed server.

--- client/pages/user.js
+++ client/pages/user.js
@@ -13,11 +13,11 @@ export default user;
 export async function getServerSideProps() {
   // Call an external API endpoint to get posts.
   const res = await fetch(
-    'http://ec2-18-223-99-203.us-east-2.compute.amazonaws.com/api/v1/posts'
+    'http://ec2-52-14-187-195.us-east-2.compute.amazonaws.com/api/v1/posts'
   );
   const posts = await res.json();
   const res2 = await fetch(
-    'http://ec2-18-223-99-203.us-east-2.compute.amazonaws.com/api/v1/posts/guests'
+    'http://ec2-52-14-187-195.us-east-2.compute.amazonaws.com/api/v1/posts/guests'
   );
   const postGuests = await res2.json();
 
